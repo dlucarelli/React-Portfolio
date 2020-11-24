e6429571f727feba044c72cf057aa27e3ccb9ab5
dlucarelli <dyllucar@gmail.com>
2020-11-24 17:04:54 -0600

    Updates header component with links to each page

---
+++ src/components/Header.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./Header.css";
+import { Link, useLocation } from "react-router-dom";
+
+function Header() {
+  const location = useLocation();
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="navbar-brand" href="#">
+        Dylan Lucarelli
+      </div>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link
+              to="/"
+              className={
+                location.pathname === "/" ? "nav-link active" : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/portfolio"
+              className={
+                location.pathname === "/portfolio"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className={
+                location.pathname === "/contact"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
