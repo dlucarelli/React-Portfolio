8bf727bb845eb3889b57e094fffec58cf2529c6a
dlucarelli <dyllucar@gmail.com>
2020-11-24 20:15:03 -0600

    Updates components to function with hooks and layered for portfolio projects

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Project from "../Project/Project";
+import "./Portfolio.css";
+
+function Portfolio() {
+  return (
+    <div className="jumbotron card-container">
+      <Project />
+    </div>
+  );
+}
+
+export default Portfolio;
