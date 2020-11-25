7772006f004ca5ad30c7876f04401ba3afe7aab6
dlucarelli <dyllucar@gmail.com>
2020-11-24 21:38:43 -0600

    updates utils and adds functionality to routing

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,3 +4,23 @@ import CardBtn from "../CardBtn";
 import CardImg from "../CardImage";
 import CardHeading from "../CardHeading";
 import "./Project.css";
+
+function Project () {
+    return (
+        <div>
+      <CardHeading />
+      <CardImg />
+      <CardBody />
+      <CardBtn
+        style={{ opacity: 1 }}
+        data-value="back"
+      />
+      <CardBtn
+        style={{ opacity: 1 }}
+        data-value="next"
+      />
+    </div>
+    );
+}
+
+export default Project;
